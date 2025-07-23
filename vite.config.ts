8564b433a942299936fcb49c6518dbc25e3ac3a2
coder-backend <ashish.adhikari7272gmail.com>
2025-07-23 19:29:01 +0530

    released

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "Medical-Internship",
+  base : "/Medical-Internship",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
