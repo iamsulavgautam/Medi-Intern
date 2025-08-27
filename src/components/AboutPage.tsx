92517b6ce81bf220e364c05b51988f5aae6225c1
Sulav Gautam <gatamsulav000@gmail.com>
2025-08-27 21:18:22 +0545

    fixation of abot us

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -38,7 +38,7 @@ const AboutPage = () => {
     },
   ];
 
-  const currentPerson = people[selectedPerson];
+  const currentPerson = people.find((person) => person.role === selectedPerson);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100">
