f6136765ada10a8db0aa3e70d0f2e4674f0ea335
Tethis Lamsal <tethislamsal3@gmail.com>
2025-08-30 21:22:32 +0545

    Update image reference for nursing program in HomePage component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -167,7 +167,7 @@ const HomePage: React.FC<HomePageProps> = ({ setCurrentPage }) => {
       duration: "4-12 weeks",
       capacity: "4-8 interns",
       location: "Maternity Hospitals",
-      image: "nurse.png",
+      image: "nurse1.png",
       description:
         "Specialized nursing program focusing on maternal and newborn care, labor and delivery, and women's reproductive health.",
       highlights: [
